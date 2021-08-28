38aefefb1620dec35911755fbeaa59ae274af9e0
red1k <erdenebayar_sh@yahoo.com>
2021-08-28 23:52:44 +0800

    major improvement

--- qr_renamer.R
+++ qr_renamer.R
@@ -1,33 +1,40 @@
 library(magick)
 library(tesseract)
 library(tidyverse)
-library(stringr)
+library(purrr)
 
 rm(list =ls())
 
+# LIST OF ALL DRIVERS
+driver_list <- readxl::read_excel('driver_list.xlsx')
+
 # IMAGE UPLOADING AND CROPPING
+img_files   <- list.files("qr/", pattern = "\\.png$", full.names = TRUE)
+all_images  <- map(img_files, image_read)
 
-img_list <- list.files("./qr", pattern = "\\.png$", full.names = TRUE)
-all_images <- lapply(img_list, image_read)
-
-width <- image_info(all_images[[1]])[2]
-
+width       <- image_info(all_images[[1]])[2]
 all_images_cropped <- lapply(all_images, image_crop, geometry_area(width = width, height = 50, y_off = 420))
 
-
 # IMAGE TO TEXT
-
 engine <- tesseract("eng")
 texts_from_img <- lapply(all_images_cropped, ocr, engine = engine)
 
+# DATA CLEANING
 final_data <- texts_from_img %>%
     str_split(pattern = " ", n = 4, simplify = TRUE) %>%
     as_tibble() %>%
     select(1, 2) %>%
     mutate(
-        full_name = paste(V1, V2, sep = " ")
+        full_name = paste(V2, V1, sep = " ") %>% 
+        str_replace("\n", "") %>% 
+        toupper()
+    ) %>% 
+    left_join(driver_list) %>%
+    select(-1, -2) %>%
+    mutate(
+        full_name = paste(Company, full_name, sep = " ")
     )
 
-names <- paste("./qr/", final_data$full_name, ".png", sep = "")
+names <- paste("qr/", final_data$full_name, ".png", sep = "")
 
-file.rename(img_list, names)
+file.rename(img_files, names)
